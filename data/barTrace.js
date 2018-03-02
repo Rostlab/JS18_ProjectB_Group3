df1d14039ec54d3db6846d4f5efc075718721d63
Ahmet Tanakol <ahmettanakol@gmail.com>
2018-03-02 13:30:37 +0100

    refactored bar chart

--- data/barTrace.js
+++ data/barTrace.js
@@ -1,101 +1,13 @@
 class BarTrace {
   constructor(x, y, color, name) {
-    this._type = 'bar';
-    this._x = x;
-    this._y = y;
-    this._marker = {
+    this.type = 'bar';
+    this.x = x;
+    this.y = y;
+    this.marker = {
       color,
     };
-    this._name = name;
-    this._width = undefined;
-  }
-
-  /**
-   * Get type
-   * @returns {string}
-   */
-  get type() {
-    return this._type;
-  }
-
-  /**
-   * Get x values of X axis
-   * @returns {*}
-   */
-  get x() {
-    return this._x;
-  }
-
-  /**
-   * Set x values of X axis
-   * @param newX
-   */
-  set x(newX) {
-    this._x = newX;
-  }
-
-  /**
-   * Get y values of Y axis
-   * @returns {*}
-   */
-  get y() {
-    return this._y;
-  }
-
-  /**
-   * Set y values of Y axis
-   * @param newY
-   */
-  set y(newY) {
-    this._y = newY;
-  }
-
-  /**
-   * Get name of trace
-   * @returns {*}
-   */
-  get name() {
-    return this._name;
-  }
-
-  /**
-   * Set name of trace
-   * @param newName
-   */
-  set name(newName) {
-    this._name = newName;
-  }
-
-  /**
-   * Get width of each x value
-   * @returns {*}
-   */
-  get width() {
-    return this._width;
-  }
-
-  /**
-   * Set width of each x value
-   * @param newWidth
-   */
-  set width(newWidth) {
-    this._width = newWidth;
-  }
-
-  /**
-   * Get color of trace
-   * @returns {*}
-   */
-  get color() {
-    return this._marker.color;
-  }
-
-  /**
-   * Set color of trace
-   * @param newColor
-   */
-  set color(newColor) {
-    this._marker.color = newColor;
+    this.name = name;
+    this.width = undefined;
   }
 
   /**
@@ -104,11 +16,11 @@ class BarTrace {
    */
   getTraceData() {
     return {
-      type: this._type,
-      x: this._x,
-      y: this._y,
-      name: this._name,
-      marker: this._marker,
+      type: this.type,
+      x: this.x,
+      y: this.y,
+      name: this.name,
+      marker: this.marker,
     };
   }
 }
