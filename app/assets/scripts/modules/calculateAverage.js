fcb5409b55a9a9df4d4869236d1d31ea050e2819
Ahmet Tanakol <ahmettanakol@gmail.com>
2018-03-27 14:42:44 +0200

    refactor, add missing unit test

--- app/assets/scripts/modules/calculateAverage.js
+++ app/assets/scripts/modules/calculateAverage.js
@@ -5,51 +5,45 @@ const ChartFactory = require('../../../helpers/chartFactory');
 const _  = require('lodash');
 
 const setBarChart = (chart, params) => {
-  _.each(chart.data, (d) => {
-    if (d.name === params.name) {
-      let numbers = {};
-      if (chart.layout.xaxis.title === params.groupAxisName) {
-        _.each(d.x, (value, index) => {
-          if (numbers.hasOwnProperty(value)) {
-            numbers[value]['count'] += 1;
-            numbers[value]['sum'] += d.y[index];
-          } else {
+  try{
+    _.each(chart.data, (d) => {
+      if (d.name === params.name) {
+        let numbers = {};
+        let targetAxis = 'x';
+        if (chart.layout.yaxis.title === params.groupAxisName) {
+          targetAxis = 'y';
+        }
+        _.each(d[targetAxis], (value, index) => {
+          if (!numbers.hasOwnProperty(value)) {
             numbers[value] = {
               count: 0,
               sum: 0
             };
-            numbers[value]['count'] += 1;
+          }
+          numbers[value]['count'] += 1;
+          if (targetAxis === 'x') {
             numbers[value]['sum'] += d.y[index];
-          }
-        });
-      } else {
-        _.each(d.y, (value, index) => {
-          if (numbers.hasOwnProperty(value)) {
-            numbers[value]['count'] += 1;
-            numbers[value]['sum'] += d.x[index];
           } else {
-            numbers[value] = {
-              count: 0,
-              sum: 0
-            };
-            numbers[value]['count'] += 1;
             numbers[value]['sum'] += d.x[index];
           }
         });
+        let newData = {
+          type: 'bar',
+          name: d.name,
+          x: [],
+          y: [],
+        };
+        _.forEach(numbers, function(value, key) {
+          newData.x.push(key);
+          newData.y.push((numbers[key].sum / numbers[key].count));
+        });
+        chart.data = [newData];
       }
-      let newData = {
-        type: 'bar',
-        name: d.name,
-        x: [],
-        y: [],
-      };
-      _.forEach(numbers, function(value, key) {
-        newData.x.push(key);
-        newData.y.push((numbers[key].sum / numbers[key].count));
-      });
-      chart.data = [newData];
-    }
-  });
+    });
+  } catch (error) {
+    return error;
+  }
+
   const chartFactory = new ChartFactory();
   const editBar = chartFactory.create('bar', chart);
 
